2225a599db888c4a4e55c7a6fb4bbe699370ee2d
Billans99 <billy.xu99@gmail.com>
2024-05-07 12:58:02 +1000

    Added npm nodemon to automatically reset the server when a change is made

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 const statisticRoutes = require('./api/routes/statistics')
 const memberRoutes = require('./api/routes/members')
 
-
+// Routes which should handle requests
 app.use('/statistics', statisticRoutes)
 app.use('/members', memberRoutes)
 
