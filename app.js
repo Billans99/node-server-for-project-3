85b181085f4b3a1eb4ef3432268ece26890f1a70
Billans99 <billy.xu99@gmail.com>
2024-05-07 21:47:51 +1000

    Finished Handling CORS errors, which is a security mechanism that prevents requests to the API by other servers and clients. Now single page applications can send requests and use our API

--- app.js
+++ app.js
@@ -11,6 +11,20 @@ app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
+// define which clients to accept (All)
+// define which headers to accept (All these header can be appended to an incoming request)
+// options request is sent first to see if it is allowed to send the request
+// res.header(Allow methods... PUT... POST... PATCH... DELETE... GET) - are allowed methods
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    )
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
+        return res.status(200).json({})
+    }   
+})
+
 // Routes which should handle requests
 app.use('/statistics', statisticRoutes)
 app.use('/members', memberRoutes)
