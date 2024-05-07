fe995ecbeb952ade8616f22b3b3161a804db44f2
Billans99 <billy.xu99@gmail.com>
2024-05-07 13:17:30 +1000

    Added default logging with morgan, and error handling in app.js

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
+
 
 const statisticRoutes = require('./api/routes/statistics')
 const memberRoutes = require('./api/routes/members')
+
+app.use(morgan('dev'))
 
 // Routes which should handle requests
 app.use('/statistics', statisticRoutes)
 app.use('/members', memberRoutes)
 
+app.use((req, res, next) => {
+    const error = new Error('Not found')
+    error.status(404)
+    next(error)
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+})
 
 module.exports = app
