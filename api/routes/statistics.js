43e6b6a97ca3aa8ece8e096c8b6c0e212926b9ac
Billans99 <billy.xu99@gmail.com>
2024-05-13 17:52:52 +1000

    Modified responses to be more accurate and provide some meta data

--- api/routes/statistics.js
+++ api/routes/statistics.js
@@ -6,7 +6,8 @@ const Statistic = require('../models/statistic')
 
 // GET request endpoint for /statistics
 // GET request to /statistics will now return all statistics, and a count of number of statistics
-// Provide meta information about the request (type, url, for more information about the object)
+// Provides meta information about the request (type, url, for more information about the object)
+// .select() before .exec() is to exclude the __v field
 router.get('/', (req, res, next) => {
     Statistic.find()
     .select('-__v')
@@ -41,6 +42,7 @@ router.get('/', (req, res, next) => {
 })
 
 // POST request endpoint for /statistics - status 201 means successful creation
+// request: type and url for more information about the object
 router.post('/', (req, res, next) => {
     const statistic = new Statistic({
         _id: new mongoose.Types.ObjectId(),
@@ -85,6 +87,8 @@ router.post('/', (req, res, next) => {
 // If the ID is found, return the document with status 200
 // If the ID is not found, return a message with status 404
 // If there is an error, return the error with status 500
+// .select() before .exec() is to exclude the __v field
+// Added request meta data to the response, with all statistics
 router.get('/:statisticId', (req, res, next) => {
     const id = req.params.statisticId
     Statistic.findById(id)
@@ -93,7 +97,14 @@ router.get('/:statisticId', (req, res, next) => {
     .then(doc => {
         console.log('From database', doc)
         if (doc) {
-            res.status(200).json(doc)
+            res.status(200).json({
+                statistic: doc,
+                request: {
+                    type: 'GET',
+                    description: 'Get all statistics',
+                    url: 'http://localhost:3000/statistics'
+                }
+            })
         } else {
             res.status(404).json({
                 message: 'No valid entry found for provided ID'
@@ -130,8 +141,13 @@ router.patch('/:statisticId', (req, res, next) => {
     Statistic.updateOne({ _id: id }, { $set: updateOps })
     .exec()
     .then(result => {
-        console.log(result)
-        res.status(200).json(result)
+        res.status(200).json({
+            message: 'Statistic updated',
+            request: {
+                type: 'GET',
+                url: 'http://localhost:3000/statistics/' + id
+            }
+        })
     })
     .catch(err => {
         console.log(err)
@@ -144,12 +160,21 @@ router.patch('/:statisticId', (req, res, next) => {
 // DELETE request by ID with message returned as JSON "Deleted Statistics"
 // colon (:) after /statistics is a dynamic parameter
 // .exec() is to create a promise
+// Added request meta data to the response including the payload required to create a new statistic
 router.delete('/:statisticId', (req, res, next) => {
     const id = req.params.statisticId
     Statistic.deleteOne({ _id: id })
     .exec()
     .then(result => {
-        res.status(200).json(result)
+        res.status(200).json({
+            message: 'Statistic deleted',
+            request: {
+                type: 'POST',
+                description: 'Create a new statistc',
+                url: 'http://localhost:3000/statistics',
+                body: { title: 'String', content: 'String', date: 'String', type: 'String', comparison: 'String', deaths: 'Number' }
+            }
+        })
     })
     .catch(err => {
         console.log(err)
