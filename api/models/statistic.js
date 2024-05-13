31e18e2ecab02bdc57f0f6b70ba9bb980a416c80
Billans99 <billy.xu99@gmail.com>
2024-05-13 16:18:57 +1000

    Added validation requirements for the data properties. title, content, date, type, and comparison properties are required, while deaths property is not required

--- api/models/statistic.js
+++ api/models/statistic.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose')
 
 const statisticSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    title: String,
-    content: String,
-    date: String,
-    type: String,
-    comparison: String,
-    deaths: Number
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    date: { type: String, required: true },
+    type: { type: String, required: true },
+    comparison: { type: String, required: true },
+    deaths: { type: Number, required: false }
 })
 
 module.exports = mongoose.model('Statistic', statisticSchema)
